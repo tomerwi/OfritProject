9e475ec207baac282d77e1f55d243f1aa560b8ed
tomerwi <tomerwi@post.bgu.ac.il>
2016-08-05 17:15:17 +0300

    delete some unused files

--- data/rScript.R
+++
@@ -1,3 +0,0 @@
-data<-read.csv("C:\\Users\\Tomer\\Documents\\GitHub\\rnn-tutorial-rnnlm\\data\\reddit-comments-2015-08.csv")
-data<-read.csv("C:\\Users\\Tomer\\Documents\\GitHub\\rnn-tutorial-rnnlm\\data\\reddit-comments-2015-08.csv")
-data3<-read.csv("C:\\Users\\Tomer\\Documents\\GitHub\\rnn-tutorial-rnnlm\\data\\speech-of-bibi.csv")
